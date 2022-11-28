dcab65a67753f82195778d0b0ce1d61f5a93baa1
ariqur <atik74734@gmail.com>
2022-11-28 21:51:51 +0600

    Fajer

--- database/migrations/2022_11_28_154056_create_teachers_table.php
+++ database/migrations/2022_11_28_154056_create_teachers_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('teachet', function (Blueprint $table) {
+        Schema::create('teachers', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
             $table->string('email')->nullable();
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('teachet');
+        Schema::dropIfExists('teachers');
     }
 };
--- database/seeders/TeacherSeeder.php
+++ database/seeders/TeacherSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use App\Models\Teacher;
 
 class TeacherSeeder extends Seeder
 {
@@ -16,38 +17,42 @@ class TeacherSeeder extends Seeder
      */
     public function run()
     {
-        $data =[
-            [
-                'name' => Str::random(10),
-                'email' => Str::random(10).'@gmail.com',
-                'address' => Str::random(10),
-            ],
-            [
-                'name' => Str::random(10),
-                'email' => Str::random(10).'@gmail.com',
-                'address' => Str::random(10),
-            ],
-            [
-                'name' => Str::random(10),
-                'email' => Str::random(10).'@gmail.com',
-                'address' => Str::random(10),
-            ],
-            [
-                'name' => Str::random(10),
-                'email' => Str::random(10).'@gmail.com',
-                'address' => Str::random(10),
-            ],
-            [
-                'name' => Str::random(10),
-                'email' => Str::random(10).'@gmail.com',
-                'address' => Str::random(10),
-            ],
-            [
-                'name' => Str::random(10),
-                'email' => Str::random(10).'@gmail.com',
-                'address' => Str::random(10),
-            ],
-        ];
-        DB::table('teachet')->insert($data);
+        Teacher::factory()->count(50)->create();
+        // $data =[
+        //     [
+        //         'name' => Str::random(10),
+        //         'email' => Str::random(10).'@gmail.com',
+        //         'address' => Str::random(10),
+        //     ],
+        //     [
+        //         'name' => Str::random(10),
+        //         'email' => Str::random(10).'@gmail.com',
+        //         'address' => Str::random(10),
+        //     ],
+        //     [
+        //         'name' => Str::random(10),
+        //         'email' => Str::random(10).'@gmail.com',
+        //         'address' => Str::random(10),
+        //     ],
+        //     [
+        //         'name' => Str::random(10),
+        //         'email' => Str::random(10).'@gmail.com',
+        //         'address' => Str::random(10),
+        //     ],
+        //     [
+        //         'name' => Str::random(10),
+        //         'email' => Str::random(10).'@gmail.com',
+        //         'address' => Str::random(10),
+        //     ],
+        //     [
+        //         'name' => Str::random(10),
+        //         'email' => Str::random(10).'@gmail.com',
+        //         'address' => Str::random(10),
+        //     ],
+        // ];
+
+            
+
+        //DB::table('teachet')->insert($data);
     }
 }
